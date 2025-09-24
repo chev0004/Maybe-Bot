ad80fa13a06a9ca614abdc6e221f6a346e594c61
chev <chevv0004@gmail.com>
2025-09-24 17:38:38 -0500

    docs: add comment

--- src/utils/helpers/gitUpdateHelper.js
+++ src/utils/helpers/gitUpdateHelper.js
@@ -21,6 +21,7 @@ export const parseGitUpdateOutput = (commitLog, gitStdout, gitStderr) => {
     })
     .join("\n");
 
+  // Hard coded just in case
   let repoUrl = "https://github.com/chev0004/Maybe-Bot";
   let branchName = "develop";
 
